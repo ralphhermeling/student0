ca9144e40a6e3c369154f997fe7a4c1c16be9450
Ralph Hermeling <ralphhermeling@gmail.com>
2025-04-14 18:48:30 +0000

    Finish homework 0

--- hw-intro/limits.c
+++ hw-intro/limits.c
@@ -3,8 +3,13 @@
 
 int main() {
     struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
+    getrlimit(RLIMIT_STACK, &lim);
+    printf("stack size: %ld\n", lim.rlim_cur);
+    struct rlimit lim2;
+    getrlimit(RLIMIT_NPROC, &lim2);
+    printf("process limit: %ld\n", lim2.rlim_cur);
+    struct rlimit lim3;
+    getrlimit(RLIMIT_NOFILE, &lim3);
+    printf("max file descriptors: %ld\n", lim3.rlim_cur);
     return 0;
 }
